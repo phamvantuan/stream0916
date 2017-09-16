c6650b08578daf059a9bbd5f2c4f48184d69161e
Tuan Pham <pvtuanhcmus@gmail.com>
2017-09-16 22:24:17 +0700

    stream many connect

--- src/app.js
+++ src/app.js
@@ -1,26 +1,3 @@
 const openStream = require('./openStream');
-const $ = require('jquery');
-// openStream();
-var Peer = require('simple-peer')
-var p = new Peer({ initiator: location.hash === '#1', trickle: false })
-p.on('signal', token => {
-	$('#txtMySignal').val(JSON.stringify(token));
-});
+openStream();
 
-p.on('connect',() => {
-setInterval(
-	function(){
-		p.send(Math.random())
-	},2000);
-});
-
-p.on('data',data => console.log('du lieu '+ data));
-
-$('#btn_connect').click(() => {
-	const friendSignal = JSON.parse($('#txtSignalToken').val());
-	p.signal(friendSignal);
-});
-
-
-
-console.log('Xin chao cac ban');
