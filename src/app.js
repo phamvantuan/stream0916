9a1b62c17dee8f34afd2d5040fe78e3e43b101df
Tuan Pham <pvtuanhcmus@gmail.com>
2017-09-16 18:41:48 +0700

    send token and data

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
 const openStream = require('./openStream');
-openStream();
+const $ = require('jquery');
+// openStream();
+var Peer = require('simple-peer')
+var p = new Peer({ initiator: location.hash === '#1', trickle: false })
+p.on('signal', token => {
+	$('#txtMySignal').val(JSON.stringify(token));
+});
+
+p.on('connect',() => {
+setInterval(
+	function(){
+		p.send(Math.random())
+	},2000);
+});
+
+p.on('data',data => console.log('du lieu '+ data));
+
+$('#btn_connect').click(() => {
+	const friendSignal = JSON.parse($('#txtSignalToken').val());
+	p.signal(friendSignal);
+});
+
+
+
+console.log('Xin chao cac ban');
